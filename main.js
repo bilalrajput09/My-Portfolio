556f2ba6b4a270000bbb1b4d755416466152f33e
bilalrajput09 <ranabilalrajput09@gmail.com>
2023-03-06 11:50:54 +0100

    Add mobile menu

---
+++ main.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".menu");
+const mobileMenu = document.querySelector("#mobile_menu_container");
+const close = document.querySelector(".close");
+
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", closeMenu);
